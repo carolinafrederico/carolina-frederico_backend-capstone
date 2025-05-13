91e46f55a95bf0a9b865fe0e219d88330bf0215b
carolinafrederico <iam@carolinafrederico.com>
2025-05-13 09:48:19 -0400

    Login Fix

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as usersController from '../controllers/user-controller.js';
-
+import * as authController from '../controllers/auth-controller.js'
 const router = express.Router();
 
 // Register and Login
-router.post('/register', usersController.registerUser);
-router.post('/login', usersController.loginUser);
+router.post('/register', authController.registerUser);
+router.post('/login', authController.loginUser);
 
 // Logout 
-router.get('/logout', usersController.logoutUser);
+router.get('/logout', authController.logoutUser);
 
 export default router;
