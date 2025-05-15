3bd1c317802e6a5ce4f3bdb3e40a8ecd3ba70f60
carolinafrederico <iam@carolinafrederico.com>
2025-05-14 20:32:58 -0400

    database

--- db/database.js
+++ db/database.js
@@ -1,22 +1,3 @@
-
-// import mongoose from "mongoose";
-// import 
-// {config} from "dotenv"
-// config();
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-   
-//     });
-//     console.log(`✅ MongoDB Connected.${mongoose.connection.n}`);
-//   } catch (error) {
-//     console.error("❌ MongoDB connection error:", error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from "mongoose";
 import { config } from "dotenv";
 
